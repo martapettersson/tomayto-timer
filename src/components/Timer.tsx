1f6c9c88362c64ad432527d5dbc17be0a0416a5b
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-08 16:26:04 +0100

    Add mode and calculation of time and percentage

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -6,21 +6,37 @@ import PauseButton from "./PauseButton";
 import PlayButton from "./PlayButton";
 import SettingsButton from "./SettingsButton";
 
+export enum Mode {
+	WORK = "work",
+	BREAK = "break",
+}
+
 const Timer: React.FC = () => {
 	const settingsInfo: any = useContext(SettingsContext);
 
 	const [isPaused, setIsPaused] = useState(true);
+	const [mode, setMode] = useState<Mode>(Mode.WORK);
+	const [secondsLeft, setSecondsLeft] = useState(0);
 
 	const isPausedRef = useRef(isPaused);
 
-	console.log(isPausedRef);
+	const totalSeconds =
+		mode === Mode.WORK
+			? settingsInfo.workMinutes * 60
+			: settingsInfo.breakMinutes * 60;
+	const percentage = Math.round((secondsLeft / totalSeconds) * 100);
+
+	let minutes: number | string = Math.floor(secondsLeft / 60);
+	let seconds: number | string = secondsLeft % 60;
+	if (minutes < 10) minutes = `0${minutes}`;
+	if (seconds < 10) seconds = `0${seconds}`;
 
 	return (
 		<div>
 			<h1>Timer</h1>
 			<CircularProgressbar
-				value={settingsInfo.workMinutes}
-				text={settingsInfo.workMinutes}
+				value={percentage}
+				text={`${minutes}:${seconds}`}
 				styles={buildStyles({
 					textColor: "#000",
 					pathColor: "red",
