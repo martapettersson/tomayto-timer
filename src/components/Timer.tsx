dbf1626b0534af17a8140e47da904b891251d03e
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-08 15:58:30 +0100

    Add react circular progressbar

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import SettingsContext from "../context/SettingsContext";
 import SettingsButton from "./SettingsButton";
 
@@ -8,6 +10,15 @@ const Timer: React.FC = () => {
 	return (
 		<div>
 			<h1>Timer</h1>
+			<CircularProgressbar
+				value={settingsInfo.workMinutes}
+				text={settingsInfo.workMinutes}
+				styles={buildStyles({
+					textColor: "#000",
+					pathColor: "red",
+					trailColor: "grey",
+				})}
+			/>
 			<div>
 				<SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
 			</div>
