662cc5fa30ba00382d5bc543fb33442366535b73
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-08 12:22:30 +0100

    Add Timer and Settings components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Settings from "./components/Settings";
+import Timer from "./components/Timer";
 
-const App = () => {
-	return <div className="App">Tomayto Timer</div>;
+const App: React.FC = () => {
+	const [showSettings, setShowSettings] = useState(false);
+	return <main>{showSettings ? <Settings /> : <Timer />}</main>;
 };
 
 export default App;
