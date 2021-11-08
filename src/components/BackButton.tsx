5cb3543d87eea044b57980bfd41bad7ca3b40d21
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-08 12:54:10 +0100

    Add back to timer button in settings component

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,5 @@
+const BackButton = (props: { onClick: () => void }) => {
+	return <button {...props}>Back</button>;
+};
+
+export default BackButton;
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import SettingsContext from "../context/SettingsContext";
+import BackButton from "./BackButton";
 
 const Settings: React.FC = () => {
-	return <div>Settings</div>;
+	const settingsInfo: any = useContext(SettingsContext);
+	return (
+		<div>
+			<h1>Settings</h1>
+			<BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+		</div>
+	);
 };
 
 export default Settings;
