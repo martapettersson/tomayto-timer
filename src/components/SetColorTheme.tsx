ecdeeacefcada81c9c4d75c20baad2a22322cb4b
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-15 21:23:02 +0100

    Add more color theme options

--- src/components/SetColorTheme.tsx
+++ src/components/SetColorTheme.tsx
@@ -12,6 +12,10 @@ const SetColorTheme: React.FC = () => {
 			<select name="colorTheme" value={colorTheme} onChange={updateColorTheme}>
 				<option value="purpleDream">Purple Dream</option>
 				<option value="darkNight">Dark Night</option>
+				<option value="solarPower">Solar Power</option>
+				<option value="greenHaven">Green Haven</option>
+				<option value="bubbleGum">Bubbel Gum</option>
+				<option value="earlGrey">Earl Grey</option>
 			</select>
 		</div>
 	);
