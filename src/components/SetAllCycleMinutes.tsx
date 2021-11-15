2838e73a748206a4e51e516a3b666678636209f1
Marta Pettersson <marta.pettersson@gmail.com>
2021-11-15 12:31:19 +0100

    Refactor SetAllCycleMinutes

--- src/components/SetAllCycleMinutes.tsx
+++ src/components/SetAllCycleMinutes.tsx
@@ -1,61 +1,67 @@
 import React, { useContext } from "react";
 import ReactSlider from "react-slider";
-
 import SettingsContext from "../context/SettingsContext";
+import { Mode } from "./Timer";
 
 const SetAllCycleMinutes: React.FC = () => {
-	const settingsInfo: any = useContext(SettingsContext);
-	const allWorkMinutes = settingsInfo.allWorkMinutes;
-	const allBreakMinutes = settingsInfo.allBreakMinutes;
-	const cycles = settingsInfo.cycles;
+	const {
+		allWorkMinutes,
+		setAllWorkMinutes,
+		allBreakMinutes,
+		setAllBreakMinutes,
+		cycles,
+		setCycles,
+	} = useContext(SettingsContext);
 
-	const handleOnSubmit = (e: any) => {
-		e.preventDefault();
-		let newCycleArray = [...cycles];
+	const updateAllCycleMinutes = (mode: Mode) => (value: number) => {
+		let newCyclesArray = [...cycles];
 
-		for (let cycle of cycles) {
-			cycle.workMinutes = allWorkMinutes;
-			cycle.breakMinutes = allBreakMinutes;
+		if (mode === Mode.WORK) {
+			setAllWorkMinutes(value);
+			for (let cycle of cycles) {
+				cycle.workMinutes = value;
+			}
+			localStorage.setItem("allWorkMinutes", JSON.stringify(value));
+		} else {
+			setAllBreakMinutes(value);
+			for (let cycle of cycles) {
+				cycle.breakMinutes = value;
+			}
+			localStorage.setItem("allBreakMinutes", JSON.stringify(value));
 		}
 
-		localStorage.setItem("allWorkMinutes", JSON.stringify(allWorkMinutes));
-		localStorage.setItem("allBreakMinutes", JSON.stringify(allBreakMinutes));
-
-		localStorage.setItem("cycles", JSON.stringify(newCycleArray));
-		settingsInfo.setCycles(newCycleArray);
+		localStorage.setItem("cycles", JSON.stringify(newCyclesArray));
+		setCycles(newCyclesArray);
 	};
 
 	return (
 		<div>
 			<h2>Set All Cycle Minutes</h2>
-			<form onSubmit={handleOnSubmit}>
-				<label>
-					Work: {allWorkMinutes < 10 ? `0${allWorkMinutes}` : allWorkMinutes}:00
-				</label>
-				<ReactSlider
-					className="slider"
-					thumbClassName="thumb"
-					trackClassName="track"
-					value={allWorkMinutes}
-					onChange={(newValue) => settingsInfo.setAllWorkMinutes(newValue)}
-					min={1}
-					max={120}
-				/>
-				<label>
-					Break:{" "}
-					{allBreakMinutes < 10 ? `0${allBreakMinutes}` : allBreakMinutes}:00
-				</label>
-				<ReactSlider
-					className="slider green"
-					thumbClassName="thumb"
-					trackClassName="track"
-					value={allBreakMinutes}
-					onChange={(newValue) => settingsInfo.setAllBreakMinutes(newValue)}
-					min={1}
-					max={120}
-				/>
-				<input type="submit" value="Set" />
-			</form>
+			<label>
+				Work: {allWorkMinutes < 10 ? `0${allWorkMinutes}` : allWorkMinutes}:00
+			</label>
+			<ReactSlider
+				className="slider"
+				thumbClassName="thumb"
+				trackClassName="track"
+				value={allWorkMinutes}
+				onChange={updateAllCycleMinutes(Mode.WORK)}
+				min={1}
+				max={120}
+			/>
+			<label>
+				Break: {allBreakMinutes < 10 ? `0${allBreakMinutes}` : allBreakMinutes}
+				:00
+			</label>
+			<ReactSlider
+				className="slider green"
+				thumbClassName="thumb"
+				trackClassName="track"
+				value={allBreakMinutes}
+				onChange={updateAllCycleMinutes(Mode.BREAK)}
+				min={1}
+				max={120}
+			/>
 		</div>
 	);
 };
